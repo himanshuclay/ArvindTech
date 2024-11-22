573f4a73c4c1faa0ff36cb04df7f02227f82f822
himanshuclay <himanshu.p@clay.co.in>
2024-11-22 15:33:47 +0530

    multiple fixes

--- src/pages/other/AdminSide/ProcessMaster/ProcessMasterInsert.tsx
+++ src/pages/other/AdminSide/ProcessMaster/ProcessMasterInsert.tsx
@@ -4,7 +4,7 @@ import { Button, Col, Form, Row, ButtonGroup } from 'react-bootstrap';
 import { useParams, useNavigate, Link } from 'react-router-dom';
 import config from '@/config';
 import Select from 'react-select';
-// import { FileUploader } from '@/components/FileUploader'
+import { FileUploader } from '@/components/FileUploader'
 import CustomSuccessToast from '../../Component/CustomSuccessToast';
 
 interface Process {
@@ -44,8 +44,7 @@ interface ModuleOwnerName {
 const EmployeeInsert = () => {
     const { id } = useParams<{ id: string }>();
     const navigate = useNavigate();
-    const [files, setFiles] = useState<File[]>([]);
-    const [error, setError] = useState('');
+
     const [showToast, setShowToast] = useState(false);
     const [toastMessage, setToastMessage] = useState("");
     const [toastVariant, setToastVariant] = useState('');
@@ -174,34 +173,7 @@ const EmployeeInsert = () => {
 
 
 
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        const selectedFiles = Array.from(event.target.files || []);
-        let newFiles: File[] = [];
-        let errorMessage = '';
 
-        // Validation for file count
-        if (selectedFiles.length > 5) {
-            errorMessage = 'You can only upload up to 5 files.';
-        } else {
-            // Validation for file size
-            newFiles = selectedFiles.filter(file => {
-                if (file.size > 10 * 1024 * 1024) {
-                    errorMessage = 'Each file must be less than 10MB.';
-                    return false;
-                }
-                return true;
-            });
-        }
-
-        // Set error message or selected files
-        if (errorMessage) {
-            setError(errorMessage);
-            setFiles([]);
-        } else {
-            setError('');
-            setFiles(newFiles);
-        }
-    };
 
 
     // Handle form field changes
@@ -328,11 +300,6 @@ const EmployeeInsert = () => {
                                     />
                                 </Form.Group>
                             </Col>
-<<<<<<< HEAD
-
-                           
-
-=======
                             <Col lg={6}>
                                 <Form.Group controlId="processFlowchart" className="mb-3">
                                     <Form.Label>Process Flowchart:</Form.Label>
@@ -361,7 +328,6 @@ const EmployeeInsert = () => {
                                         console.log('Files uploaded:', files);
                                     }}
                                 /> */}
->>>>>>> 22b80cf9878a1301417e72cc6f563bb4fe388065
                             <Col lg={6}>
                                 <Form.Group controlId="misExempt" className="mb-3">
                                     <Form.Label>MIS Exempt:</Form.Label>
@@ -427,36 +393,6 @@ const EmployeeInsert = () => {
                                         placeholder="Select Process Owner Name"
                                         required
                                     />
-                                </Form.Group>
-                            </Col>
-                            <Col lg={6}>
-                                <Form.Group controlId="processFlowchart" className="mb-3">
-                                    <Form.Label>Process Flowchart</Form.Label>
-                                    <div className='input-file-custom'>
-                                        <label htmlFor="processFlowchart" className="file-upload w-100">
-                                            <div className='d-flex justify-content-between align-items-center'>
-                                                <i className="ri-upload-cloud-2-line h1 text-muted m-0"></i>
-                                                <div>
-                                                    <h4>Drop files here or click to upload.</h4>
-                                                    <span className='fs-13 text-muted'>(.jpeg, .jpg, .png, .pdf Note: You can only select up to 5 files, each up to 10MB.)</span>
-                                                </div>
-                                            </div>
-                                        </label>
-                                        <input
-                                            id="processFlowchart    "
-                                            type="file"
-                                            multiple
-                                            onChange={handleFileChange}
-                                            accept=".png,.jpeg,.jpg,.pdf"
-                                            style={{ display: 'none' }}
-                                        />
-                                        {error && <p style={{ color: 'red' }}>{error}</p>}
-                                        <ul>
-                                            {files.map((file, index) => (
-                                                <li key={index}>{file.name}</li>
-                                            ))}
-                                        </ul>
-                                    </div>
                                 </Form.Group>
                             </Col>
 
