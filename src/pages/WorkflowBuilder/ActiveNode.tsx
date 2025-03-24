53e0dcf6625233c6fa4768424c51751efb3dab6c
sandeep-sana <sandeep.s@clay.co.in>
2025-03-24 11:22:03 +0530

    block value

--- src/pages/WorkflowBuilder/ActiveNode.tsx
+++ src/pages/WorkflowBuilder/ActiveNode.tsx
@@ -1,12 +1,41 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Editor from "../FormBuilder/Editor";
 import { FIELD, PROPERTY } from "../FormBuilder/Constant/Interface";
 import axios from "axios";
 import config from "@/config";
 import { toast } from "react-toastify";
+import STAFF_ALLOCATION_PLAN from "./DynamicSegment/STAFF_ALLOCATION_PLAN";
+import APPOINTMENT from "./DynamicSegment/APPOINTMENT";
+import NEW_APPOINTMENT from "./DynamicSegment/NEW_APPOINTMENT";
+import OLD_STAFF_TRANSFER from "./DynamicSegment/OLD_STAFF_TRANSFER";
+import INDUCTION from "./DynamicSegment/INDUCTION";
+import UPDATE_EMPLOYEE from "./DynamicSegment/UPDATE_EMPLOYEE";
+import APPOINTMENT_LETTER from "./DynamicSegment/APPOINTMENT_LETTER";
+import ASSIGN_TASK from "./DynamicSegment/ASSIGN_TASK";
 
-const ActiveNode = ({ activeNode, activeTaskId, setActiveNode }: { activeNode: any; activeTaskId: number; setActiveNode: (value: any) => void; }) => {
-    const [form, setForm] = useState<FIELD>({ ...activeNode.data.form, editMode: true });
+const ActiveNode = ({ activeNode, activeTaskId, setActiveNode, completedNodes, setCompletedNodes }: { activeNode: any; activeTaskId: number; setActiveNode: (value: any) => void; completedNodes: any; setCompletedNodes: (value: any) => void; }) => {
+    const [form, setForm] = useState<FIELD>(
+        activeNode.data?.form?.blocks?.length
+            ? {
+                ...activeNode.data.form,
+                editMode: true, // You probably want to override editMode to true if an existing form is found
+            }
+            : {
+                name: '',
+                blocks: [],
+                blockCount: 0,
+                editMode: true,
+                rules: [],
+                advance: {
+                    backgroundColor: '',
+                    color: '',
+                },
+            }
+    );
+    const [dynamicComponent, setDynamicComponent] = useState<string>(activeNode.data?.form?.blocks?.length ? '' : activeNode.data.form);
+    const componentRefMap = useRef<{ [key: string]: any }>({});
+
+
     const [property, setProperty] = useState<PROPERTY>({
         label: '',
         id: '',
@@ -32,28 +61,83 @@ const ActiveNode = ({ activeNode, activeTaskId, setActiveNode }: { activeNode: a
         try {
             activeNode.data['blockValue'] = blockValue;
             activeNode.data['status'] = "completed";
+            activeNode.data.form = form;
+            if (componentRefMap.current[dynamicComponent]) {
+                activeNode.data['blockValue'] = componentRefMap.current[dynamicComponent]?.[dynamicComponent]?.();
+            }
             const query = {
                 id: activeTaskId,
                 jsonInput: JSON.stringify(activeNode)
             }
-            console.log('query', query);
+            console.log('query', activeNode.data.blockValue);
             const response = await axios.post(
                 `${config.API_URL_ACCOUNT}/ProcessInitiation/UpdateTemplateJson`,
                 query
             );
-            if(response.data.isSuccess){
+            if (response.data.isSuccess) {
                 toast.success(response.data.message);
                 setActiveNode("");
             }
 
+            
         } catch (error) {
             console.log(error)
         }
     }
+    useEffect(() => {
+        // Debugging: log the current completedNodes
+        console.log('completedNodes', completedNodes);
+        if (completedNodes.length) {
+
+            // Ensure that `completeNode.data` and `completeNode.data.form` exist
+            const updatedNodes = completedNodes.map((completeNode: any) => ({
+                ...completeNode,
+                data: {
+                    ...completeNode.data, // Ensure completeNode.data is spread
+                    form: {
+                        ...completeNode.data?.form, // Safely spread form if it exists
+                        editMode: false,
+                    }
+                }
+            }));
+
+            // Debugging: log the updatedNodes
+            console.log(updatedNodes);
+
+            // Update state with the new array of completed nodes
+            setCompletedNodes(updatedNodes);
+        }
+    }, []);
+    const componentMap: { [key: string]: React.FC<any> } = {
+        STAFF_ALLOCATION_PLAN,
+        APPOINTMENT,
+        NEW_APPOINTMENT,
+        OLD_STAFF_TRANSFER,
+        INDUCTION,
+        UPDATE_EMPLOYEE,
+        APPOINTMENT_LETTER,
+        ASSIGN_TASK,
+    };
+
     return (
         <div>
-            {/* {JSON.stringify(activeNode)} */}
-            <Editor form={form} setForm={setForm} property={property} setProperty={setProperty} blockValue={blockValue} setBlockValue={setBlockValue} isShowSave={false} />
+            {completedNodes.map((completeNode: any) => (
+                <>
+                    <Editor form={completeNode.data.form} setForm={setForm} property={property} setProperty={setProperty} blockValue={completeNode.data.blockValue} setBlockValue={setBlockValue} isShowSave={false} isPreview={true} />
+                </>
+            ))}
+            {form?.blocks?.length && (
+                <Editor form={form} setForm={setForm} property={property} setProperty={setProperty} blockValue={blockValue} setBlockValue={setBlockValue} isShowSave={false} />
+            )}
+            {dynamicComponent && componentMap[dynamicComponent] && (
+                React.createElement(componentMap[dynamicComponent], {
+                    ref: (instance: any) => {
+                        if (instance) {
+                            componentRefMap.current[dynamicComponent] = instance;
+                        }
+                    }
+                })
+            )}
             <button type="button" onClick={handleSumbitTask}>Save</button>
         </div>
     )
