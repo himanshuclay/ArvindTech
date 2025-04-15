c547abb433a82c1dfd16e69efb4fdc4bef3e625a
webtechwithsumit <sumitkumar10107@gmail.com>
2025-04-15 18:10:31 +0530

    update

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -97,11 +97,16 @@ const Dashboard = () => {
 		}
 	}, [startDate, endDate]);
 
-	if (!data) {
-		return <p>Loading...</p>;
-	}
+	// if (!data) {
+	// 	return <p>Loading...</p>;
+	// }
 
-	const { p1, p2, totalTasks, tasksNotDone, taskNotDoneOnTime } = data;
+	const p1 = data?.p1 ?? 0;
+	const p2 = data?.p2 ?? 0;
+	const totalTasks = data?.totalTasks ?? 0;
+	const tasksNotDone = data?.tasksNotDone ?? 0;
+	const taskNotDoneOnTime = data?.taskNotDoneOnTime ?? 0;
+
 
 	const taskCompleted = totalTasks - tasksNotDone;
 	const taskChartData = {
@@ -162,247 +167,256 @@ const Dashboard = () => {
 		<>
 			<PageBreadcrumb title="Welcome!" />
 
-			<Row className="mt-3 text-dark">
-				<Col lg={6}>
-					<Row className='mb-3'>
-						<Col>
-							<div className='d-flex justify-content-between align-items-center py-3  px-3 bg-white rounded shadow-sm'>
-								<div>
-									<p className='mb-1'>Doers Managed By Me</p>
-									<h3>146</h3>
+
+			{!data ? (
+				<div className='loader-container'>
+					<div className="loader"></div>
+					<div className='mt-2'>Please Wait!</div>
+				</div>
+			) : (<>
+
+
+				<Row className="mt-3 text-dark">
+					<Col lg={6}>
+						<Row className='mb-3'>
+							<Col>
+								<div className='d-flex justify-content-between align-items-center py-3  px-3 bg-white rounded shadow-sm'>
+									<div>
+										<p className='mb-1'>Doers Managed By Me</p>
+										<h3>146</h3>
+									</div>
+									<div><i className='ri-group-line fs-18'></i></div>
+
 								</div>
-								<div><i className='ri-group-line fs-18'></i></div>
+							</Col>
 
-							</div>
-						</Col>
+							<Col>
+								<div className='d-flex justify-content-between align-items-center py-3  px-3 bg-white rounded shadow-sm'>
+									<div>
+										<p className='mb-1'>Modules With My Tasks</p>
+										<h3>{0}</h3>
+									</div>
+									<div><i className="ri-logout-box-r-line fs-18"></i></div>
 
-						<Col>
-							<div className='d-flex justify-content-between align-items-center py-3  px-3 bg-white rounded shadow-sm'>
-								<div>
-									<p className='mb-1'>Modules With My Tasks</p>
-									<h3>{0}</h3>
 								</div>
-								<div><i className="ri-logout-box-r-line fs-18"></i></div>
-
-							</div>
-						</Col>
-					</Row>
-					<Row>
-						<Col>
-							<div className='d-flex justify-content-between align-items-center py-3  px-3 bg-white rounded shadow-sm'>
-								<div>
-									<p className='mb-1'>Processes With My Tasks</p>
-									<h3>{0}</h3>
-								</div>
-								<div><i className="ri-git-merge-fill fs-18"></i></div>
-
-							</div>
-						</Col>
-						<Col>
-							<div className='d-flex justify-content-between align-items-center py-3  px-3 bg-white rounded shadow-sm'>
-								<div>
-									<p className='mb-1'>Total Tasks</p>
-									<h3>{0}</h3>
-								</div>
-								<div><i className="ri-check-double-line fs-18"></i></div>
-
-							</div>
-						</Col>
-					</Row>
-				</Col>
-				<Col lg={6} className='text-primary'>
-					<h5 className='text-dark'>Quick Access</h5>
-					<Row>
-						<Col lg={3} sm={6} className='text-center my-2'>
-							<div className='dashboard-quick' style={{ background: '#9433eb' }}>{0}</div>
-							<h6>Tasks Due Till Today</h6>
-						</Col>
-						<Col lg={3} sm={6} className='text-center my-2'>
-							<div className='dashboard-quick' style={{ background: '#14b8a7' }}>{0}</div>
-							<h6>Future Tasks</h6>
-						</Col>
-						<Col lg={3} sm={6} className='text-center my-2'>
-							<div className='dashboard-quick' style={{ background: '#87efac' }}>{0}</div>
-							<h6>Tasks Assigned Today</h6>
-						</Col>
-						<Col lg={3} sm={6} className='text-center my-2'>
-							<div className='dashboard-quick' style={{ background: '#da2727' }}>{0}</div>
-							<h6>Tasks Pending On Me</h6>
-						</Col>
-					</Row>
-					<Row>
-						<Col lg={3} sm={6} className='text-center my-2'>
-							<div className='dashboard-quick' style={{ background: '#00bdd5' }}>{0}</div>
-							<h6>Pending Tasks Without Extension</h6>
-						</Col>
-						<Col lg={3} sm={6} className='text-center my-2'>
-							<div className='dashboard-quick' style={{ background: '#fca6a5' }}>{0}</div>
-							<h6>Pending Tasks {'>'} 7 Days</h6>
-						</Col>
-						<Col lg={3} sm={6} className='text-center my-2'>
-							<div className='dashboard-quick' style={{ background: '#f87270' }}>{0}</div>
-							<h6>pending Tasks {'>'} 15 Days</h6>
-						</Col>
-						<Col lg={3} sm={6} className='text-center my-2'>
-							<div className='dashboard-quick' style={{ background: '#da2727' }}>{0}</div>
-							<h6>Pending Tasks {'>'} 30 Days</h6>
-						</Col>
-					</Row>
-
-				</Col>
-
-
-			</Row>
-
-
-			<Row className='mt-4'>
-
-				<Col lg={6}>
-					<Form>
-						<Row>
-							<Col lg={6}>
-								<Form.Group controlId="startDate" className="mb-3">
-									<Flatpickr
-										options={{
-											enableTime: false,
-											dateFormat: "Y-m-d",
-											time_24hr: false,
-											maxDate: new Date(), // Restrict future dates
-											disable: [
-												function (date) {
-													// return true to disable
-													return (date.getDay() !== 1);
-
-												}
-											],
-											locale: {
-												"firstDayOfWeek": 1 // start week on Monday
-											}
-
-										}}
-										value={startDate || new Date()} // Ensure correct type
-										onChange={handleStartDateChange}
-										placeholder="Select Start Date (Past Monday)"
-										className="form-control"
-									/>
-								</Form.Group>
-							</Col>
-
-							<Col lg={6}>
-								<Form.Group controlId="endDate" className="mb-3">
-									<Flatpickr
-										options={{
-											enableTime: false,
-											dateFormat: "Y-m-d",
-											time_24hr: false,
-											minDate: startDate,
-											maxDate: startDate ? new Date(new Date(startDate).setDate(new Date(startDate).getDate() + 6)) : "", // Set max date to Sunday
-										}}
-										value={endDate || new Date()} // Ensure correct type
-										placeholder="Auto-filled End Date (Sunday)"
-										className="form-control"
-										disabled
-									/>
-								</Form.Group>
-							</Col>
-
-
-							<Col lg={6}>
-								<Form.Group controlId="departmentName" className="mb-3">
-									<Select
-										name="departmentName"
-										// value={departmentList.find((emp) => emp.name === employee.departmentName)}
-										// onChange={(selectedOption) => {
-										// 	setEmployee({
-										// 		...employee,
-										// 		departmentName: selectedOption?.name || "",
-										// 	});
-										// }}
-										// getOptionLabel={(emp) => emp.name}
-										// getOptionValue={(emp) => emp.name}
-										// options={departmentList}
-										isSearchable={true}
-										placeholder="Choose User"
-									/>
-
-								</Form.Group>
-							</Col>
-
-							<Col lg={6}>
-								<Form.Group controlId="departmentName" className="mb-3">
-									<Select
-										name="departmentName"
-										isSearchable={true}
-										placeholder="Select Project User"
-									/>
-
-								</Form.Group>
-							</Col>
-							<Col lg={6}>
-								<Form.Group controlId="departmentName" className="mb-3">
-									<Select
-										name="departmentName"
-										isSearchable={true}
-										placeholder="Select Module"
-									/>
-
-								</Form.Group>
-							</Col>
-							<Col lg={6}>
-								<Form.Group controlId="departmentName" className="mb-3">
-									<Select
-										name="departmentName"
-										isSearchable={true}
-										placeholder="Select Process"
-									/>
-								</Form.Group>
-							</Col>
-							<Col className='text-center'>
-								<Button onClick={fetchMISData}>
-									Apply
-								</Button>
 							</Col>
 						</Row>
-					</Form>
-				</Col>
+						<Row>
+							<Col>
+								<div className='d-flex justify-content-between align-items-center py-3  px-3 bg-white rounded shadow-sm'>
+									<div>
+										<p className='mb-1'>Processes With My Tasks</p>
+										<h3>{0}</h3>
+									</div>
+									<div><i className="ri-git-merge-fill fs-18"></i></div>
 
-				<Col lg={6} className='mt-2'>
-					<Row>
-						<Col lg={6}>
-							<div className="pt-2 text-dark">
-								<div className="text-center taskpercentage">
-									<div className="bg-primary text-white px-2 mb-2 py-1 rounded">
-										<i className="ri-group-line fs-18"></i>
+								</div>
+							</Col>
+							<Col>
+								<div className='d-flex justify-content-between align-items-center py-3  px-3 bg-white rounded shadow-sm'>
+									<div>
+										<p className='mb-1'>Total Tasks</p>
+										<h3>{0}</h3>
 									</div>
-									<h4>Total Tasks: {totalTasks}</h4>
-									<div className="d-flex justify-content-between">
-										<p className="mb-1 fs-11">Completed: {taskCompleted}</p>
-										<p className="mb-1 fs-11">Not Done: {tasksNotDone}</p>
-										<p className="mb-1 fs-11">Not Done on Time: {taskNotDoneOnTime}</p>
-									</div>
-									<div style={{ height: "150px" }}>
-										<Doughnut data={taskChartData} options={taskChartOptions} />
+									<div><i className="ri-check-double-line fs-18"></i></div>
+
+								</div>
+							</Col>
+						</Row>
+					</Col>
+					<Col lg={6} className='text-primary'>
+						<h5 className='text-dark'>Quick Access</h5>
+						<Row>
+							<Col lg={3} sm={6} className='text-center my-2'>
+								<div className='dashboard-quick' style={{ background: '#9433eb' }}>{0}</div>
+								<h6>Tasks Due Till Today</h6>
+							</Col>
+							<Col lg={3} sm={6} className='text-center my-2'>
+								<div className='dashboard-quick' style={{ background: '#14b8a7' }}>{0}</div>
+								<h6>Future Tasks</h6>
+							</Col>
+							<Col lg={3} sm={6} className='text-center my-2'>
+								<div className='dashboard-quick' style={{ background: '#87efac' }}>{0}</div>
+								<h6>Tasks Assigned Today</h6>
+							</Col>
+							<Col lg={3} sm={6} className='text-center my-2'>
+								<div className='dashboard-quick' style={{ background: '#da2727' }}>{0}</div>
+								<h6>Tasks Pending On Me</h6>
+							</Col>
+						</Row>
+						<Row>
+							<Col lg={3} sm={6} className='text-center my-2'>
+								<div className='dashboard-quick' style={{ background: '#00bdd5' }}>{0}</div>
+								<h6>Pending Tasks Without Extension</h6>
+							</Col>
+							<Col lg={3} sm={6} className='text-center my-2'>
+								<div className='dashboard-quick' style={{ background: '#fca6a5' }}>{0}</div>
+								<h6>Pending Tasks {'>'} 7 Days</h6>
+							</Col>
+							<Col lg={3} sm={6} className='text-center my-2'>
+								<div className='dashboard-quick' style={{ background: '#f87270' }}>{0}</div>
+								<h6>pending Tasks {'>'} 15 Days</h6>
+							</Col>
+							<Col lg={3} sm={6} className='text-center my-2'>
+								<div className='dashboard-quick' style={{ background: '#da2727' }}>{0}</div>
+								<h6>Pending Tasks {'>'} 30 Days</h6>
+							</Col>
+						</Row>
+
+					</Col>
+
+
+				</Row>
+
+
+				<Row className='mt-4'>
+
+					<Col lg={6}>
+						<Form>
+							<Row>
+								<Col lg={6}>
+									<Form.Group controlId="startDate" className="mb-3">
+										<Flatpickr
+											options={{
+												enableTime: false,
+												dateFormat: "Y-m-d",
+												time_24hr: false,
+												maxDate: new Date(), // Restrict future dates
+												disable: [
+													function (date) {
+														// return true to disable
+														return (date.getDay() !== 1);
+
+													}
+												],
+												locale: {
+													"firstDayOfWeek": 1 // start week on Monday
+												}
+
+											}}
+											value={startDate || new Date()} // Ensure correct type
+											onChange={handleStartDateChange}
+											placeholder="Select Start Date (Past Monday)"
+											className="form-control"
+										/>
+									</Form.Group>
+								</Col>
+
+								<Col lg={6}>
+									<Form.Group controlId="endDate" className="mb-3">
+										<Flatpickr
+											options={{
+												enableTime: false,
+												dateFormat: "Y-m-d",
+												time_24hr: false,
+												minDate: startDate,
+												maxDate: startDate ? new Date(new Date(startDate).setDate(new Date(startDate).getDate() + 6)) : "", // Set max date to Sunday
+											}}
+											value={endDate || new Date()} // Ensure correct type
+											placeholder="Auto-filled End Date (Sunday)"
+											className="form-control"
+											disabled
+										/>
+									</Form.Group>
+								</Col>
+
+
+								<Col lg={6}>
+									<Form.Group controlId="departmentName" className="mb-3">
+										<Select
+											name="departmentName"
+											// value={departmentList.find((emp) => emp.name === employee.departmentName)}
+											// onChange={(selectedOption) => {
+											// 	setEmployee({
+											// 		...employee,
+											// 		departmentName: selectedOption?.name || "",
+											// 	});
+											// }}
+											// getOptionLabel={(emp) => emp.name}
+											// getOptionValue={(emp) => emp.name}
+											// options={departmentList}
+											isSearchable={true}
+											placeholder="Choose User"
+										/>
+
+									</Form.Group>
+								</Col>
+
+								<Col lg={6}>
+									<Form.Group controlId="departmentName" className="mb-3">
+										<Select
+											name="departmentName"
+											isSearchable={true}
+											placeholder="Select Project User"
+										/>
+
+									</Form.Group>
+								</Col>
+								<Col lg={6}>
+									<Form.Group controlId="departmentName" className="mb-3">
+										<Select
+											name="departmentName"
+											isSearchable={true}
+											placeholder="Select Module"
+										/>
+
+									</Form.Group>
+								</Col>
+								<Col lg={6}>
+									<Form.Group controlId="departmentName" className="mb-3">
+										<Select
+											name="departmentName"
+											isSearchable={true}
+											placeholder="Select Process"
+										/>
+									</Form.Group>
+								</Col>
+								<Col className='text-center'>
+									<Button onClick={fetchMISData}>
+										Apply
+									</Button>
+								</Col>
+							</Row>
+						</Form>
+					</Col>
+
+					<Col lg={6} className='mt-2'>
+						<Row>
+							<Col lg={6}>
+								<div className="pt-2 text-dark">
+									<div className="text-center taskpercentage">
+										<div className="bg-primary text-white px-2 mb-2 py-1 rounded">
+											<i className="ri-group-line fs-18"></i>
+										</div>
+										<h4>Total Tasks: {totalTasks}</h4>
+										<div className="d-flex justify-content-between">
+											<p className="mb-1 fs-11">Completed: {taskCompleted}</p>
+											<p className="mb-1 fs-11">Not Done: {tasksNotDone}</p>
+											<p className="mb-1 fs-11">Not Done on Time: {taskNotDoneOnTime}</p>
+										</div>
+										<div style={{ height: "150px" }}>
+											<Doughnut data={taskChartData} options={taskChartOptions} />
+										</div>
 									</div>
 								</div>
-							</div>
-						</Col>
+							</Col>
 
-						{/* P1 & P2 Performance Chart */}
-						<Col lg={6}>
-							<div className="pt-2 text-dark">
-								<div className="text-center taskpercentage">
-									<div className="bg-primary text-white px-2 mb-2 py-1 rounded">
-										<i className="ri-group-line fs-18"></i>
-									</div>
-									<h4>P1: {p1}%</h4>
-									<h4>P2: {p2}%</h4>
-									<div style={{ height: "125px" }}>
-										<Doughnut data={performanceChartData} options={performanceChartOptions} />
+							{/* P1 & P2 Performance Chart */}
+							<Col lg={6}>
+								<div className="pt-2 text-dark">
+									<div className="text-center taskpercentage">
+										<div className="bg-primary text-white px-2 mb-2 py-1 rounded">
+											<i className="ri-group-line fs-18"></i>
+										</div>
+										<h4>P1: {p1}%</h4>
+										<h4>P2: {p2}%</h4>
+										<div style={{ height: "125px" }}>
+											<Doughnut data={performanceChartData} options={performanceChartOptions} />
+										</div>
 									</div>
 								</div>
-							</div>
-						</Col>
-						{/* <Col lg={4}>
+							</Col>
+							{/* <Col lg={4}>
 							<div className='pt-2 text-dark '>
 								<div className='text-center  taskpercentage'>
 									<div className='bg-primary text-white px-2 mb-2 py-1 rounded '>
@@ -426,10 +440,12 @@ const Dashboard = () => {
 
 							</div>
 						</Col> */}
-					</Row>
+						</Row>
 
-				</Col>
-			</Row>
+					</Col>
+				</Row>
+			</>)
+			}
 
 		</>
 	)
