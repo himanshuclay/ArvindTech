4d01cdb3e5fa822c154429438123a80daf804354
himanshuclay <himanshu.p@clay.co.in>
2025-05-01 14:50:13 +0530

    fixes

--- vite.config.ts
+++ vite.config.ts
@@ -21,9 +21,9 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  build: {
-      rollupOptions: {
-        external: ['regenerator-runtime/runtime'],
-      },
-  },
+  // build: {
+  //     rollupOptions: {
+  //       external: ['regenerator-runtime/runtime'],
+  //     },
+  // },
 });
