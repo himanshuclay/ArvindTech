a03654e9cd378e08fda962fd8dc0e3fff7614bed
sandeep-sana <sandeep.s@clay.co.in>
2025-05-05 16:58:34 +0530

    active node

--- src/pages/FormBuilder/Property/Property.tsx
+++ src/pages/FormBuilder/Property/Property.tsx
@@ -241,6 +241,19 @@ const Property: React.FC<Props> = ({ form, setForm, property, setProperty, remov
                         <option value="Y-m-d">YYYY/MM/DD</option>
                         <option value="F, Y">M, YYYY(March, 2025)</option>
                         <option value="mmm/yy">MMM/YY</option>
+                        <option value="dd-mm-yyyy">d-m-Y</option>
+                        <option value="DD-MM-YYYY">D-m-Y</option>
+                        <option value="d-m-yyyy">j-n-Y</option>
+                        <option value="dd-mm-YYYY">d-m-Y</option>
+                        <option value="yyyy-mm-dd">Y-m-d</option>
+                        <option value="MM-DD-YYYY">m-d-Y</option>
+                        <option value="D, d M Y">D, d M Y</option>
+                        <option value="F j, Y">F j, Y</option>
+                        <option value="l, d F Y">l, d F Y</option>
+                        <option value="HH:MM">H:i</option>
+                        <option value="HH:MM:SS">H:i:S</option>
+                        <option value="YYYY-MM-DD HH:MM">Y-m-d H:i</option>
+                        <option value="dd-mm-yyyy HH:MM">d-m-Y H:i</option>
                     </select>
                 </div>
             )}
